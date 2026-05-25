7eeda899eb2dd802cc160b92e145a9bb53d3e2b9
kiegosoto-creator <kiegosoto@gmail.com>
2026-05-24 23:03:20 -0600

    Update header comments in Origen.py
    Actualize el inicio de la documentación según el formato general que fue provisto por la profesora durante el curso.

--- Origen.py
+++ Origen.py
@@ -1,3 +1,8 @@
-# Tarea-programada-2-Diego-y-Cristopher-
-Donador de sangre.
-print("a")
+#Elaborado por: Cristopher Abel Jara Salazar, y Diego Kim Soto.
+#Fecha de elaboracion: 22/5/2026 6:00 PM.
+#Ultima modificacion: 24/5/2026 11:00 PM.
+#Version: 0.0.0
+
+#Definicion de funciones.
+
+#Funcion principal.
