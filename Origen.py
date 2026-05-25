de554d911ea7c4b84980bee022b5c798f0e6430e
kiegosoto-creator <kiegosoto@gmail.com>
2026-05-24 23:18:10 -0600

    Add main menu function for user interaction.
    Agregue el menu principal. Ahora todo lo que hace falta hacer es definir de manera clara la estructura de los datos, para esto, pasare un periodo extenso, asegurándome de como funcionan, antes de crear los procesos. Aun así, esto facilita, y le da los huesos al cuerpo del programa.

--- Origen.py
+++ Origen.py
@@ -5,4 +5,34 @@
 
 #Definicion de funciones.
 
+def menuPrincipal():
+  while True:
+    print('\nMenu Principal de...')
+    print('\n1.Insertar donador.')
+    print('2.Generar donadores')
+    print('3.Actualizar datos del donador.')
+    print('4.Eliminnar donador.')
+    print('5.Insertar lugar de donacion segun provincia.')
+    print('6.Reportes.')
+    print('7.Salir')
+    opcion=input('\n:_')
+    if opcion=='7':
+      print('\nSaliendo del programa...')
+      break
+    elif opcion=='6':
+      print('')
+    elif opcion=='5':
+      print('')
+    elif opcion=='4':
+      print('')
+    elif opcion=='3':
+      print('')
+    elif opcion=='2':
+      print('')
+    elif opcion=='1':
+      print('')
+    else:
+      print('\nOpcion no valida.')
+      input('\nIngrese (ENTER), para continuar.')
+
 #Funcion principal.
